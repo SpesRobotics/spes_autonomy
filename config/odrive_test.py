db3ef52bc9e9ec0319a4af20f5bd5a5c00f6f414
Darko Lukic <lukicdarkoo@gmail.com>
2022-09-07 21:13:11 +0000

    Add package

--- config/odrive_test.py
+++ config/odrive_test.py
@@ -6,9 +6,9 @@ from odrive.enums import *
 def main():
     odrv0 = odrive.find_any()
     print('Setting a sample speed...')
-    odrv0.axis1.controller.config.vel_gain = 0.01
-    odrv0.axis1.controller.config.vel_integrator_gain = 0.3
-    odrv0.axis1.motor.config.current_lim = 30.0
+    odrv0.axis1.controller.config.vel_gain = 0.11
+    odrv0.axis1.controller.config.vel_integrator_gain = 1.6
+    odrv0.axis1.motor.config.current_lim = 4.0
 
     odrv0.axis1.requested_state = AXIS_STATE_ENCODER_INDEX_SEARCH
     odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
