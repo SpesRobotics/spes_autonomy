42ab48990e65df77f0cd1d5a55e29d79cf86679d
Darko Lukic <lukicdarkoo@gmail.com>
2022-09-07 22:46:15 +0200

    two axis

--- config/odrive_config.py
+++ config/odrive_config.py
@@ -34,7 +34,7 @@ def save_configuration(odrv0, erase=False):
     return odrv_instance
 
 
-def main():
+def main(axis='axis1'):
     do_anticogging = ('--anticogging' in sys.argv)
 
     odrv0 = odrive.find_any()
@@ -42,60 +42,61 @@ def main():
     print('Voltage:', odrv0.vbus_voltage)
     odrv0 = save_configuration(odrv0, erase=True)
 
-    odrv0.axis1.motor.config.pole_pairs = 15
+    getattr(odrv0, axis).motor.config.pole_pairs = 15
 
-    odrv0.axis1.motor.config.resistance_calib_max_voltage = 4
-    odrv0.axis1.motor.config.requested_current_range = 25
-    odrv0.axis1.motor.config.current_control_bandwidth = 100
-    odrv0.axis1.motor.config.current_lim = 4.0
-    odrv0.axis1.motor.config.calibration_current = 4.0
+    getattr(odrv0, axis).motor.config.resistance_calib_max_voltage = 4
+    getattr(odrv0, axis).motor.config.requested_current_range = 25
+    getattr(odrv0, axis).motor.config.current_control_bandwidth = 100
+    getattr(odrv0, axis).motor.config.current_lim = 4.0
+    getattr(odrv0, axis).motor.config.calibration_current = 4.0
 
-    odrv0.axis1.encoder.config.mode = ENCODER_MODE_HALL
-    odrv0.axis1.encoder.config.cpr = 90
-    odrv0.axis1.encoder.config.bandwidth = 100
-    odrv0.axis1.encoder.config.use_index = True
+    getattr(odrv0, axis).encoder.config.mode = ENCODER_MODE_HALL
+    getattr(odrv0, axis).encoder.config.cpr = 90
+    getattr(odrv0, axis).encoder.config.bandwidth = 100
+    getattr(odrv0, axis).encoder.config.use_index = True
 
-    odrv0.axis1.controller.config.pos_gain = 1.0
-    odrv0.axis1.controller.config.vel_gain = 0.02
-    odrv0.axis1.controller.config.vel_integrator_gain = 0.1
-    odrv0.axis1.controller.config.vel_limit = 10
-    odrv0.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+    getattr(odrv0, axis).controller.config.pos_gain = 1.0
+    getattr(odrv0, axis).controller.config.vel_gain = 0.02
+    getattr(odrv0, axis).controller.config.vel_integrator_gain = 0.1
+    getattr(odrv0, axis).controller.config.vel_limit = 10
+    getattr(odrv0, axis).controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
     odrv0 = save_configuration(odrv0)
 
     print('Calibrating motor...')
-    odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-    while odrv0.axis1.requested_state != AXIS_STATE_UNDEFINED or odrv0.axis1.current_state != AXIS_STATE_IDLE:
+    getattr(odrv0, axis).requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+    while getattr(odrv0, axis).requested_state != AXIS_STATE_UNDEFINED or getattr(odrv0, axis).current_state != AXIS_STATE_IDLE:
         time.sleep(1)
-    print('Motor calibration complete', odrv0.axis1.error)
-    odrv0.axis1.motor.config.pre_calibrated = True
-    odrv0.axis1.encoder.config.pre_calibrated = True
+    print('Motor calibration complete', getattr(odrv0, axis).error)
+    getattr(odrv0, axis).motor.config.pre_calibrated = True
+    getattr(odrv0, axis).encoder.config.pre_calibrated = True
     odrv0 = save_configuration(odrv0)
 
     if do_anticogging:
         print('Calibrating anti-cogging...')
-        odrv0.axis1.encoder.config.use_index = True
-        odrv0.axis1.controller.config.control_mode = CONTROL_MODE_POSITION_CONTROL
-        odrv0.axis1.controller.config.input_mode = INPUT_MODE_PASSTHROUGH
-        odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-        odrv0.axis1.controller.config.anticogging.calib_pos_threshold = 1.0
-        odrv0.axis1.controller.config.anticogging.calib_vel_threshold = 1.0
-        odrv0.axis1.controller.start_anticogging_calibration()
-        while odrv0.axis1.controller.config.anticogging.calib_anticogging:
+        getattr(odrv0, axis).encoder.config.use_index = True
+        getattr(odrv0, axis).controller.config.control_mode = CONTROL_MODE_POSITION_CONTROL
+        getattr(odrv0, axis).controller.config.input_mode = INPUT_MODE_PASSTHROUGH
+        getattr(odrv0, axis).requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        getattr(odrv0, axis).controller.config.anticogging.calib_pos_threshold = 1.0
+        getattr(odrv0, axis).controller.config.anticogging.calib_vel_threshold = 1.0
+        getattr(odrv0, axis).controller.start_anticogging_calibration()
+        while getattr(odrv0, axis).controller.config.anticogging.calib_anticogging:
             time.sleep(1)
         print('Anti-cogging calibration complete')
-        odrv0.axis1.controller.config.anticogging.pre_calibrated = True
+        getattr(odrv0, axis).controller.config.anticogging.pre_calibrated = True
         odrv0 = save_configuration(odrv0)
 
     print('Setting a sample speed...')
-    odrv0.axis1.requested_state = AXIS_STATE_ENCODER_INDEX_SEARCH
-    odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    odrv0.axis1.controller.config.input_mode = INPUT_MODE_PASSTHROUGH
-    odrv0.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
-    odrv0.axis1.controller.input_vel = 0.1
+    getattr(odrv0, axis).requested_state = AXIS_STATE_ENCODER_INDEX_SEARCH
+    getattr(odrv0, axis).requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+    getattr(odrv0, axis).controller.config.input_mode = INPUT_MODE_PASSTHROUGH
+    getattr(odrv0, axis).controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+    getattr(odrv0, axis).controller.input_vel = 0.1
     print('Axis in closed loop control')
     time.sleep(10)
-    odrv0.axis1.requested_state = AXIS_STATE_IDLE
+    getattr(odrv0, axis).requested_state = AXIS_STATE_IDLE
 
 
 if __name__ == '__main__':
-    main()
+    main('axis0')
+    main('axis1')
