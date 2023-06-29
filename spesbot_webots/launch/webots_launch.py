f1bcec3dcc01b0613f932eb0306edb040e9ff48c
Marija Golubovic <marijagolubovic16@gmail.com>
2023-06-29 18:52:34 +0200

    Try to launch webots-no1

--- spesbot_webots/launch/webots_launch.py
+++ spesbot_webots/launch/webots_launch.py
@@ -1,10 +1,40 @@
 import os
 import pathlib
+import yaml
+import launch
 
 from ament_index_python.packages import get_package_share_directory
-import launch
 from launch_ros.actions import Node
 from webots_ros2_driver.webots_launcher import WebotsLauncher
+from launch_ros.actions import Node
+
+def get_controller_spawners(controller_params_file):
+    with open(controller_params_file, 'r') as f:
+        controller_params = yaml.safe_load(f)
+
+    controller_names = list(controller_params['controller_manager']['ros__parameters'].keys())
+
+    # Create controller spawners
+    controller_spawners = []
+    for controller_name in controller_names:
+        if controller_name in ['update_rate', 'publish_rate']:
+            continue
+
+        controller_spawners.append(Node(
+            package='controller_manager',
+            executable='spawner',
+            output='screen',
+            emulate_tty=True,
+            arguments=[
+                controller_name,
+                '--controller-manager-timeout',
+                '50',
+                '--controller-manager',
+                'controller_manager',
+            ])
+        )
+    return controller_spawners
+
 
 
 def generate_launch_description():
@@ -13,20 +43,49 @@ def generate_launch_description():
 
     package_dir = get_package_share_directory('spesbot_webots')
 
+    controller_params = os.path.join(get_package_share_directory('spesbot_hardware'),
+                             'resource', 'ros2_control.yaml')
 
+    robot_description = pathlib.Path(
+        os.path.join(package_dir, 'resource', 'robot_description.urdf')).read_text()
+    print("JSNDXMSNM:", robot_description)
     webots = WebotsLauncher(world=os.path.join(package_dir, 'data',
-                                               'worlds', 'spesbot.wbt'), ros2_supervisor=False)
+                                               'worlds', 'spesbot.wbt'), ros2_supervisor=True)
+                                    
 
-   
+    webots_robot_driver = Node(
+        package='webots_ros2_driver',
+        executable='driver',
+        output='screen',  # debugging
+        emulate_tty=True,  # debugging
+        parameters=[
+            {
+                'use_sim_time': True,
+                'robot_description': robot_description
+            },
+            controller_params
+        ],
+        remappings=[
+            ('/diffdrive_controller/cmd_vel_unstamped', 'cmd_vel'),
+            ('/diffdrive_controller/odom', 'odom'),
+            ('/tf', 'tf'),
+            ('/tf_static', 'tf_static'),
+            ('/scan', 'scan'),
+            ('/scan/point_cloud', 'scan/point_cloud'),
+        ],
+        ros_arguments=['--log-level', 'warn'],
+    )
 
     # Standard ROS 2 launch description
     return launch.LaunchDescription([
         # Start the Webots node
         webots,
+        webots._supervisor,
+        webots_robot_driver,
         # This action will kill all nodes once the Webots simulation has exited
         launch.actions.
         RegisterEventHandler(event_handler=launch.event_handlers.OnProcessExit(
             target_action=webots,
             on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
         ))
-    ])
+    ] + get_controller_spawners(controller_params))
