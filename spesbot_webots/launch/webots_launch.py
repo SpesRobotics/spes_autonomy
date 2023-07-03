e65602a5993926a50c966d54ad7a7ce30c9df850
Marija Golubovic <marijagolubovic16@gmail.com>
2023-07-03 13:49:15 +0200

    Add camera in simulation and remap ros topic

--- spesbot_webots/launch/webots_launch.py
+++ spesbot_webots/launch/webots_launch.py
@@ -43,7 +43,8 @@ def generate_launch_description():
         ],
         remappings=[
             ('/diffdrive_controller/cmd_vel_unstamped', 'cmd_vel'),
-            ('/diffdrive_controller/odom', 'odom')
+            ('/diffdrive_controller/odom', 'odom'),
+            ('/spesbot/camera', 'camera')
         ],
         ros_arguments=['--log-level', 'warn'],
         additional_env={'WEBOTS_CONTROLLER_URL': 'spesbot'},
---
+++ spesbot_webots/src/image_subscriber.py
@@ -0,0 +1,23 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class ImageSubscriber(Node):
+    def __init__(self):
+        super().__init__('image_subscriber')
+        self.subscription = self.create_subscription(Image, 'camera', self.image_callback, 10)
+        self.cv_bridge = CvBridge()
+
+    def image_callback(self, msg):
+        cv_image = self.cv_bridge.imgmsg_to_cv2(msg)
+
+        cv2.imshow('Image', cv_image)
+        cv2.waitKey(1)
+
+# def main(args=None):
+#     rclpy.init(args=args)
+#     image_subscriber = ImageSubscriber()
+#     rclpy.spin(image_subscriber)
+#     rclpy.shutdown()
