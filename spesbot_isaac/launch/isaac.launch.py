4b5b8968bd385afb98b479aeb8ec682be404715f
MNikoliCC <nikolicmilos3610@gmail.com>
2024-04-30 09:10:04 +0000

    Add .usd and .usda files, rename the .yaml file, format files, rename the topics of topic based hardware interface

--- spesbot_isaac/launch/isaac.launch.py
+++ spesbot_isaac/launch/isaac.launch.py
@@ -6,41 +6,48 @@ import xacro
 
 
 def generate_launch_description():
-    package_dir = get_package_share_directory('spesbot_description')
-    xacro_file = os.path.join(package_dir, 'urdf', 'spesbot.xacro')
+    package_dir = get_package_share_directory("spesbot_description")
+    xacro_file = os.path.join(package_dir, "urdf", "spesbot.xacro")
     robot_description = xacro.process_file(xacro_file).toxml()
 
     config = os.path.join(
-        get_package_share_directory('spesbot_isaac'),
-        'config',
-        'spesbot_control.yaml'
+        get_package_share_directory("spesbot_isaac"), "config", "controllers.yaml"
     )
 
     # Define the launch description
-    ld = LaunchDescription([
-        Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        parameters=[{'robot_description': robot_description}, config],
-        remappings=[
-            ('/diff_drive_controller/cmd_vel_unstamped', 'cmd_vel'),
-            ('/diff_drive_controller/odom', 'odom'),
-        ],
-        output='screen',
-    ),
-        Node(
-        package='controller_manager',
-        executable='spawner',
-        output='screen',
-        arguments=['joint_state_broadcaster', '--controller-manager-timeout', '50'],
-    ),
-        Node(
-        package='controller_manager',
-        executable='spawner',
-        output='screen',
-        arguments=['diff_drive_controller', '--controller-manager-timeout', '50'],
-    ),
-
-    ])
+    ld = LaunchDescription(
+        [
+            Node(
+                package="controller_manager",
+                executable="ros2_control_node",
+                parameters=[{"robot_description": robot_description}, config],
+                remappings=[
+                    ("/diff_drive_controller/cmd_vel_unstamped", "cmd_vel"),
+                    ("/diff_drive_controller/odom", "odom"),
+                ],
+                output="screen",
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                output="screen",
+                arguments=[
+                    "joint_state_broadcaster",
+                    "--controller-manager-timeout",
+                    "50",
+                ],
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                output="screen",
+                arguments=[
+                    "diff_drive_controller",
+                    "--controller-manager-timeout",
+                    "50",
+                ],
+            ),
+        ]
+    )
 
     return ld
